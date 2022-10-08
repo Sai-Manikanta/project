1e2220640908758b8cef4d9b126a8b0f815bedf8
mani333007 <mani333007@gmail.com>
2022-10-08 23:15:35 +0530

    Pagination login, signup added

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,13 @@
+import { Routes, Route, Navigate } from 'react-router-dom'
+import { Login, Signup } from './pages'
+
 function App() {
     return (
-        <div>App</div>
+        <Routes>
+            <Route path="/" element={<Navigate to="/login" />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/signup" element={<Signup />} />
+        </Routes>
     )
 }
 
